ee234b48569617838252d3ba3b608706fa2729bb
Muhammad Aadil <adil.isani1@gmail.com>
2023-12-29 21:16:31 +0500

    header-component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </>
   )
 }
