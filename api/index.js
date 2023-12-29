8a04fd0cda6f523b35f93c1bdc216c6a9b47a578
Adil Isani <adil.isani1@gmail.com>
2023-12-29 23:26:50 +0500

    User modal

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
-    console.log(err)
+    console.log(err);
 })
 
 app.listen(port, () => {
