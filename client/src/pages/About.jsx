d2cc0f1de41f390b8f8633b0c0231458623fedbb
Adil Isani <adil.isani1@gmail.com>
2024-01-25 16:56:46 +0500

    home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className='px-5 py-5 mx-auto max-w-6xl mb-3 md:mb-5'>
+
+            <h1 className="text-center text-2xl md:text-3xl mb-5 font-extrabold text-slate-700"> About This App</h1>
+            <p className="text-center text-base md:text-2xl mb-5 font-normal text-slate-700">
+                Welcome to the About page of my authentication application! 🌐 This app is designed to provide a secure and user-friendly authentication experience using the MERN stack (MongoDB, Express, React, Node.js). Let's dive into some key aspects of this project:
+            </p>
+
+            <span className="text-center text-2xl font-bold text-red-700">## Project Overview</span>
+            <p className="mt-5 text-slate-700 text-base lg:text-2xl">
+                This authentication app serves as a foundation for user authentication in web applications. It's built with modern web development practices, ensuring scalability, security, and a seamless user experience.
+            </p>
+
+            <p className="mt-5 text-slate-700 text-base md:text-2xl">
+                <p className='text-orange-600 text-2xl font-bold mb-2 '>## Development Process</p>
+                The development process prioritized clean and maintainable code, adhering to best practices. Regular testing and code reviews were conducted to ensure the reliability and security of the application.
+
+            </p>
+
+            <p className="mt-5 text-slate-700 mb-4 text-base md:text-2xl">
+                <p className='text-purple-600 text-2xl font-bold mb-2'>## Get Involved</p>
+                Feel free to explore the source code available on [GitHub](link-to-your-repository). If you have any feedback, suggestions, or would like to contribute, we welcome your involvement in making this project even better.
+
+            </p>
+
+            <p className=" text-slate-700 mt-2 font-medium text-base md:text-2xl">
+                Thank you for being a part of our journey! 🚀
+            </p>
+        </div>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='px-5 py-5 mx-auto max-w-6xl mb-3 md:mb-5'>
+
+            <h1 className="text-center text-2xl md:text-3xl mb-5 font-extrabold text-slate-700">Welcome to My Auth App!</h1>
+            <p className="text-center text-base md:text-2xl mb-5 font-normal text-slate-700">
+                Thank you for exploring my authentication application! 🚀 This app has been developed using the MERN stack, incorporating various technologies to create a secure and seamless user authentication experience.
+            </p>
+
+            <span className="text-center text-2xl font-bold text-red-700">Technologies Used:</span>
+            <p className="mt-5 text-slate-700 text-base lg:text-2xl">
+                <p className='text-green-600 text-2xl font-bold mb-2'>### Frontend</p>
+                - **React Vite:** The frontend of this application is built with React using Vite, a fast and opinionated build tool for modern web development.
+            </p>
+
+            <p className="mt-5 text-slate-700 text-base md:text-2xl">
+                <p className='text-orange-600 text-2xl font-bold mb-2 '>### Backend</p>
+                - **MongoDB:** The application utilizes MongoDB as the database to store user information securely.
+                - **Express:** Express.js is used as the backend framework to handle server-side logic and API endpoints.
+                - **Node.js:** The backend is powered by Node.js, providing a scalable and efficient runtime environment.
+            </p>
+
+            <p className="mt-5 text-slate-700 mb-4 text-base md:text-2xl">
+                <p className='text-purple-600 text-2xl font-bold mb-2'>### Authentication Features</p>
+                - **Bcrypt:** Passwords are securely hashed using Bcrypt to protect user credentials.
+                - **JWT (JSON Web Tokens):** JWT is employed for secure and stateless user authentication, enhancing the overall security of the application.
+            </p>
+
+            <span className='text-blue-600 text-2xl font-bold '>## Get Started:</span>
+
+            <p className=" text-slate-700 mt-2 text-base md:text-2xl">
+                To explore the functionalities of this authentication app, follow these steps:
+
+                1. Clone the repository.
+                2. Install dependencies using `npm install` in both the client and server directories.
+                3. Configure your MongoDB connection in the server's `.env` file.
+                4. Run the server using `npm start` in the server directory.
+                5. Run the client using `npm run dev` in the client directory.
+
+                Feel free to explore the code and experiment with the features! If you have any questions or feedback, don't hesitate to reach out. Happy coding! 🚀
+            </p>
+        </div>
     )
 }
 
